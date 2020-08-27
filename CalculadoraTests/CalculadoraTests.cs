f5960ed85aabd5b77cd4b535ea131b1299f46be7
Valdinei Almeida <>
2020-08-27 20:52:08 -0300

    üpdate calculadora.cs

--- CalculadoraTests/CalculadoraTests.cs
+++ CalculadoraTests/CalculadoraTests.cs
@@ -8,6 +8,14 @@ namespace CalculadoraTests
         [Fact]
         public void Calculadora_Soma_DeveRetornarSoma()
         {
+            //Arrange
+            var calculadora = new Calculadora.Calculadora();
+
+            //Act
+            var resultado = calculadora.Soma(5, 2);
+
+            //Assert
+            Assert.Equal(expected: 4, actual:resultado);
         }
     }
 }
